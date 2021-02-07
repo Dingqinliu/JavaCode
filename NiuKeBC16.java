f081865bfc01727a56661d2c386ecfbf75ea06fe
Dingqinliu <dingqinliu@163.com>
2021-02-07 22:21:47 +0800

    小练习之字母转数字

---
+++ NiuKeBC16.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+//字母转数字 ASCII码 输入c 输出99
+public class NiuKeBC16 {
+    public static void main(String[] args) {
+        Scanner ch=new Scanner(System.in);
+        char c='c';
+        System.out.println((int)ch);
+    }
+}
