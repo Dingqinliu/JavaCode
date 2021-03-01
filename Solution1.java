8fdb641b7fa97bd393ef5134eb19770f5b79afb6
Dingqinliu <dingqinliu@163.com>
2021-03-01 12:02:23 +0800

    小练习之有序数组目标元素查找/插入

--- Solution1.java
+++ Solution1.java
@@ -1,8 +1,8 @@
-public class Solution {
+public class Solution1 {
     //给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引
     // 如果目标值不存在于数组中，返回它将会被按顺序插入的位置
     // 假设数组中无重复元素
-    public static int searchInsert(int[] nums,int target){
+    public int searchInsert(int[] nums,int target){
         for (int i=0;i<nums.length;i++){
             if (nums[i]>=target){
                 return i;
@@ -14,7 +14,8 @@ public class Solution {
     public static void main(String[] args) {
         int[] array={1,3,5,7,9};
         int target=2;
-        int i=searchInsert(array, target);
+        Solution1 a=new Solution1();
+        int i=a.searchInsert(array, target);
         System.out.println(i);
     }
 }
