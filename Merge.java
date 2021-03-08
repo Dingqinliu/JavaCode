e8dca4659a83ad54bd1193e2ed6b588786affaa0
Dingqinliu <dingqinliu@163.com>
2021-03-08 19:05:01 +0800

    小练习之有序合并两个有序数组到其中一个数组中

---
+++ Merge.java
@@ -0,0 +1,18 @@
+package Homework;
+
+public class Merge {
+    //给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+    //初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+
+    private static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p = m-- + n-- - 1;//从后往前找
+        while (m >= 0 && n >= 0) {
+            nums1[p--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        }
+
+        while (n >= 0) {
+            nums1[p--] = nums2[n--];
+        }
+
+    }
+}
